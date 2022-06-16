3ee783ccf5a63cbc7250548434afc555b24a2bc5
Isaí Arellano <isai.arellano@outlook.com>
2022-06-16 10:57:04 -0500

    Initial Commit

---
+++ Camera-Custom-SwiftUI-Combine/Camera_Custom_SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_Custom_SwiftUI_CombineApp.swift
+//  Camera-Custom-SwiftUI-Combine
+//
+//  Created by Isaí on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Camera_Custom_SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
